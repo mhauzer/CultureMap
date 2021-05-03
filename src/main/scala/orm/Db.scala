8cfcc83871dd5e58cfdbcf076b3bacbeb0a006f7
Michał Hauzer <mhauzer@gmail.com>
2021-05-03 06:54:02 +0200

    reformatting

--- src/main/scala/orm/Db.scala
+++ src/main/scala/orm/Db.scala
@@ -3,8 +3,11 @@ package orm
 import java.sql.{Connection, DriverManager}
 
 object Db {
-  var url: String = ""
   classOf[org.postgresql.Driver]
-  def setUrl(url: String): Unit = this. url = url
+
+  var url: String = ""
+
+  def setUrl(url: String): Unit = this.url = url
+
   def connection: Connection = DriverManager.getConnection(url)
 }
